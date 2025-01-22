0c7bf67c19da380f9849cc674944ba36b260d0e2
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2025-01-22 14:21:32 +0100

    feat: 4th milestone complete

--- controller/postsController.js
+++ controller/postsController.js
@@ -8,7 +8,7 @@ const index = (req, res) => {
 
     connection.query(sql, (err, results) => {
 
-        if(err) {
+        if (err) {
             return res.status(500).json({
                 message: 'Internal Server Error'
             })
@@ -25,32 +25,54 @@ const index = (req, res) => {
 
 // Show
 const show = (req, res) => {
-   
+
+    const id = req.params.id;
+    const sql = 'SELECT * FROM `posts` WHERE id = ?;';
+
+    connection.query(sql, [id], (err, results) => {
+
+        if (err) {
+            return res.status(500).json({
+                message: 'Internal Server Error'
+            })
+        } else if (results.length === 0) {
+            return res.status(404).json({
+                message: 'Post Not Found'
+            })
+        } else {
+            return res.status(200).json({
+                status: 'success',
+                data: results,
+            })
+        }
+
+    })
+    
 };
 
 // // Store
 // const store = (req, res) => {
-    
+
 // };
 
 // // Update
 // const update = (req, res) => {
-    
+
 // };
 
 // // Modify
 // const modify = (req, res) => {
-    
+
 // };
 
 // Destroy
 const destroy = (req, res) => {
-    
+
     const id = req.params.id;
     const sql = 'DELETE FROM `posts` WHERE id = ?;';
 
     connection.query(sql, [id], (err) => {
-        
+
         if (err) {
             return res.status(500).json({
                 message: 'Internal Server Error'
